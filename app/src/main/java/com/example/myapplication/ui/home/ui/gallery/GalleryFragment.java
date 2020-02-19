4255418d4120af4af0c4d94da1e870fe1d3945fb
jcamilovelandiab <jcamilovelandiab@gmail.com>
2020-02-19 05:23:09 -0500

    back button in gallery and gps fragment are done

--- app/src/main/java/com/example/myapplication/ui/home/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/ui/gallery/GalleryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -16,10 +17,20 @@ import com.example.myapplication.R;
 
 public class GalleryFragment extends Fragment {
 
+    Button btn_back;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        btn_back = root.findViewById(R.id.gallery_btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().popBackStack();
+            }
+        });
+
         return root;
     }
 }
